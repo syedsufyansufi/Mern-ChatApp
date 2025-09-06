6aa6535176e0498c794b10f980c27244f0e9559f
syedsufyansufi <syedsufiyan787@gmail.com>
2025-09-06 17:47:01 +0530

    new commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,11 +29,11 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(_dirname, "../frontend/dist")));
-}
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
-});
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server Running At http://localhost:${PORT}`);
